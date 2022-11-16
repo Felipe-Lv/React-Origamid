a224b9ca1a7596770c57478241a1b79d34bb0268
Felipe Alves <fbarros.silva2000@gmail.com>
2022-11-16 17:17:45 -0400

    aula 0208 react completo Arrays

---
+++ meuapp/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+    const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+    return (
+        <ul>{filmes.map(filme => (<li key={filme}>{filme}</li>))}</ul>
+    )
+}
+
+export default App;
